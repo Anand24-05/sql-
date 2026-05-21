960b831ddba6d1d48c70f2c74f92c6d13c64b212
Anand24-05 <anandnawle05@gmail.com>
2026-05-21 23:10:11 +0530

    Created table

---
+++ Query1.sql
@@ -0,0 +1,9 @@
+show Databases;
+use Database_Name;
+
+create table employee 
+(id int, name varchar(20), salary int, city varchar(20) );
+
+insert into employee 
+values (101, 'ram', 10000, 'pune'),
+(102, 'ritesh', 50000, 'mumbai');
